aa243b8e2d4e57d3d83c95ab18db0513c4290ecf
Jan <thetisiboth@gmail.com>
2021-11-17 15:04:58 +0100

    Refactor infosection description

--- olca-app/src/org/openlca/app/results/comparison/InfoSection.java
+++ olca-app/src/org/openlca/app/results/comparison/InfoSection.java
@@ -1,7 +1,6 @@
 package org.openlca.app.results.comparison;
 
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -26,50 +25,51 @@ public class InfoSection {
 		if (setup == null || setup.productSystem() == null)
 			return;
 		Composite comp = UI.formSection(body, tk, M.GeneralInformation);
-		Label description = new Label(comp, SWT.WRAP);
-		description.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
-		String descriptionText;
+////		Label description = new Label(comp, SWT.NONE);
+////		description.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, true, false));
+		String descriptionText = "";
 		if (setup.impactMethod() == null) {
-			description.setText(
-					"Please choose an impact method, so we are able to display some information about the selected processes. In order to do so, you have to restart the calculation.");
-			return;
+			descriptionText += "Please choose an impact method, so we are able to display some information about the selected processes. In order to do so, you have to restart the calculation.";
 		}
 		if (target.equals(TargetCalculationEnum.PRODUCT_SYSTEM))
 			descriptionText = "This diagram display the environmental impact of a product system or a process for each chosen impact categories."
-					+ " One horizontal bar contains all the processes contributions from the product system or process that we chose, for one impact category.";
-		else
-			descriptionText = "This diagram display the environmental impact for a chosen impact category for each product system from the project."
-					+ " One horizontal bar contains all the processes contributions from a product system from the project, for the chosen impact category.";
-		descriptionText += " The same processes contributions are linked together, from 2 bar which are next to each other. "
-				+ "The processes contributions are sorted by ascending order, from left to right.\n";
-		if (target.equals(TargetCalculationEnum.PRODUCT_SYSTEM))
-			descriptionText += "In the setting section, you can select the impact categories that you want to analyze."
+					+ " One horizontal bar contains all the processes contributions from the product system or process that we chose, for one impact category."
+					+ " The same processes contributions are linked together, from 2 bar which are next to each other. "
+					+ "The processes contributions are sorted by ascending order, from left to right.\n"
+					+ "In the setting section, you can select the impact categories that you want to analyze."
 					+ " Initialy, they are all selected, with the alphabetical order. If you want to select/unselect impact categories,"
-					+ " the order in which you select them will change the order in which the contributions are displayed.";
-		else
-			descriptionText += "In the setting section, you can select one impact category at a time. that you want to analyze.";
-		descriptionText += " You can change the way the processes contributions are colored by "
-				+ "(either by processes, by location or by process category). "
-				+ "It means that if you select Location, then 2 processes which have the same location will have the same color. "
-				+ "You can also choose to highlight a specific process category, "
-				+ "and change the color that will be displayed for the chosen process category. "
-				+ "You can finally set up a cut-off limit, which indicate how much processes you want to be hidden. "
-				+ "The higher the number is, the less processes are displayed.";
-		description.setText(descriptionText);
+					+ " the order in which you select them will change the order in which the contributions are displayed."
+					+ " You can change the way the processes contributions are colored by "
+					+ "(either by processes, by location or by process category). "
+					+ "It means that if you select Location, then 2 processes which have the same location will have the same color. "
+					+ "You can also choose to highlight a specific process category, "
+					+ "and change the color that will be displayed for the chosen process category. "
+					+ "You can finally set up a cut-off limit, which indicate how much processes you want to be hidden. "
+					+ "The higher the number is, the less processes are displayed.";
+		UI.formLabel(comp, descriptionText);
+//		text(comp, tk, "Description", descriptionText);
+
 	}
 
 	static void create(Composite body, FormToolkit tk, Project project) {
 		if (project == null)
 			return;
 		Composite comp = UI.formSection(body, tk, M.GeneralInformation);
-		project.variants.stream().forEach(v -> link(comp, M.ProductSystem, v.productSystem));
+		String descriptionText = "This diagram display the environmental impact for a chosen impact category for each product system from the project."
+				+ " One horizontal bar contains all the processes contributions from a product system from the project, for the chosen impact category."
+				+ " The same processes contributions are linked together, from 2 bar which are next to each other. "
+				+ "The processes contributions are sorted by ascending order, from left to right.\n"
+				+ "In the setting section, you can select one impact category at a time. that you want to analyze."
+				+ " You can change the way the processes contributions are colored by "
+				+ "(either by processes, by location or by process category). "
+				+ "It means that if you select Location, then 2 processes which have the same location will have the same color. "
+				+ "You can also choose to highlight a specific process category, "
+				+ "and change the color that will be displayed for the chosen process category. "
+				+ "You can finally set up a cut-off limit, which indicate how much processes you want to be hidden. "
+				+ "The higher the number is, the less processes are displayed.";
+		UI.formLabel(comp, descriptionText);
+//		text(comp, tk, "Description", descriptionText);
 
-		if (project.impactMethod != null) {
-			link(comp, M.ImpactAssessmentMethod, project.impactMethod);
-		}
-		if (project.nwSet != null) {
-			text(comp, tk, M.NormalizationAndWeightingSet, project.nwSet.name);
-		}
 	}
 
 	static void text(Composite comp, FormToolkit tk, String label, String val) {
